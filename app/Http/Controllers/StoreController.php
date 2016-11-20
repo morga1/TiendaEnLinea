141b1df871d5bed485a485b1dfb10aca089ae1f6
Unknown <romanmorga@gmail.com>
2016-11-20 13:49:51 -0600

    Creacion de Controller Tienda y la Vista Index
    Se creo storecontroller.php , se modifico route.php y se creo la vista store/index.php

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Product;
+
+class StoreController extends Controller
+{
+ 	public function index()
+ 	{
+
+ 		$products = Product::all();
+ 		//dd($products);
+ 		return view('store.index', compact('products'));
+
+ 	}
+}
