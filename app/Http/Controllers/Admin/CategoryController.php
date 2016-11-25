b6d991d5bd59377af65e854039bc858345f55d93
Unknown <romanmorga@gmail.com>
2016-11-25 13:27:36 -0600

    Se corrigio CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -31,9 +31,24 @@ class CategoryController extends Controller
      *
      * @return Response
      */
-    public function store()
+    public function store(Request $request)
     {
-        return request->all();
+        //return $request->all();
+        $this->validate($request, [
+          'name' => 'required|unique:categories|max:255',
+          'color' => 'required',
+        ]);
+        
+        $category = Category::create([
+            'name' => $request->get('name'),
+            'slug' => str_slug($request->get('name')),
+            'description' => $request->get('description'),
+            'color' => $request->get('color')
+        ]);
+        
+        $message = $category ? 'Categoría agregada correctamente!' : 'La Categoría NO pudo agregarse!';
+        
+        return redirect()->route('admin.category.index')->with('message', $message);
     }
     /**
      * Display the specified resource.
@@ -43,7 +58,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-  
+        return $category;
     }
     /**
      * Show the form for editing the specified resource.
@@ -53,7 +68,7 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-
+        return view('admin.category.edit', compact('category'));
     }
     /**
      * Update the specified resource in storage.
@@ -63,7 +78,18 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-
+        $this->validate($request, [
+          'name' => 'required|max:255',
+          'color' => 'required',
+        ]);
+        $category->fill($request->all());
+        $category->slug = str_slug($request->get('name'));
+        
+        $updated = $category->save();
+        
+        $message = $updated ? 'Categoría actualizada correctamente!' : 'La Categoría NO pudo actualizarse!';
+        
+        return redirect()->route('admin.category.index')->with('message', $message);
     }
     /**
      * Remove the specified resource from storage.
@@ -73,6 +99,10 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-
+        $deleted = $category->delete();
+        
+        $message = $deleted ? 'Categoría eliminada correctamente!' : 'La Categoría NO pudo eliminarse!';
+        
+        return redirect()->route('admin.category.index')->with('message', $message);
     }
 }
